cfb15befebd46ca798aa5411cbaf026b7d712512
andr30z <51346021+andr30z@users.noreply.github.com>
2023-03-17 09:07:14 -0300

    fix: playlistDTO  copyProperties call

--- backend/src/main/java/com/fgc/combo/companion/service/impl/PlaylistServiceImpl.java
+++ backend/src/main/java/com/fgc/combo/companion/service/impl/PlaylistServiceImpl.java
@@ -23,78 +23,69 @@ import org.webjars.NotFoundException;
 @Service
 public class PlaylistServiceImpl implements PlaylistService {
 
-  private final PlaylistRepository playlistRepository;
-  private final PlaylistMapper playlistMapper;
-  private final UserService userService;
-  private final PlaylistComboService playlistComboService;
+    private final PlaylistRepository playlistRepository;
+    private final PlaylistMapper playlistMapper;
+    private final UserService userService;
+    private final PlaylistComboService playlistComboService;
 
-  public PlaylistServiceImpl(
-    PlaylistRepository playlistRepository,
-    PlaylistMapper playlistMapper,
-    UserService userService,
-    PlaylistComboService playlistComboService
-  ) {
-    this.playlistRepository = playlistRepository;
-    this.playlistMapper = playlistMapper;
-    this.userService = userService;
-    this.playlistComboService = playlistComboService;
-  }
+    public PlaylistServiceImpl(
+            PlaylistRepository playlistRepository,
+            PlaylistMapper playlistMapper,
+            UserService userService,
+            PlaylistComboService playlistComboService) {
+        this.playlistRepository = playlistRepository;
+        this.playlistMapper = playlistMapper;
+        this.userService = userService;
+        this.playlistComboService = playlistComboService;
+    }
 
-  @Override
-  @Transactional
-  public PlaylistResponseDTO create(CreatePlaylistDTO playlistDTO) {
-    User currentUser = userService.me();
+    @Override
+    @Transactional
+    public PlaylistResponseDTO create(CreatePlaylistDTO playlistDTO) {
+        User currentUser = userService.me();
 
-    Playlist playlist = playlistMapper.toPlaylist(playlistDTO);
-    playlist.setOwner(currentUser);
-    Playlist createdPlaylist = this.playlistRepository.save(playlist);
+        Playlist playlist = playlistMapper.toPlaylist(playlistDTO);
+        playlist.setOwner(currentUser);
+        Playlist createdPlaylist = this.playlistRepository.save(playlist);
 
-    this.playlistComboService.addAllCombosToPlaylist(
-        createdPlaylist,
-        playlistDTO.getCombos()
-      );
+        this.playlistComboService.addAllCombosToPlaylist(
+                createdPlaylist,
+                playlistDTO.getCombos());
 
-    return playlistMapper.toPlaylistReponseDTO(createdPlaylist);
-  }
+        return playlistMapper.toPlaylistReponseDTO(createdPlaylist);
+    }
 
-  @Override
-  public PlaylistResponseDTO update(Long id, UpdatePlaylistDTO playlistDTO) {
-    User currentUser = userService.me();
+    @Override
+    public PlaylistResponseDTO update(Long id, UpdatePlaylistDTO playlistDTO) {
+        User currentUser = userService.me();
 
-    Playlist playlist = playlistRepository
-      .findById(id)
-      .orElseThrow(() -> new NotFoundException("Playlist not found!"));
+        Playlist playlist = playlistRepository
+                .findById(id)
+                .orElseThrow(() -> new NotFoundException("Playlist not found!"));
 
-    if (
-      playlist.getOwner().getId() != currentUser.getId()
-    ) throw new OperationNotAllowedException(
-      "You can't edit a playlist that is not yours!"
-    );
+        if (playlist.getOwner().getId() != currentUser.getId())
+            throw new OperationNotAllowedException(
+                    "You can't edit a playlist that is not yours!");
 
-    BeanUtils.copyProperties(playlist, playlistDTO);
-    return playlistMapper.toPlaylistReponseDTO(
-      this.playlistRepository.save(playlist)
-    );
-  }
+        BeanUtils.copyProperties(playlistDTO, playlist);
+        return playlistMapper.toPlaylistReponseDTO(
+                this.playlistRepository.save(playlist));
+    }
 
-  @Override
-  public PlaylistResponseDTO getByIdAndCurrentUser(Long id) {
-    Playlist playlist =
-      this.playlistRepository.findByIdAndOwner(id, this.userService.me())
-        .orElseThrow(() -> new NotFoundException("Playlist not found!"));
+    @Override
+    public PlaylistResponseDTO getByIdAndCurrentUser(Long id) {
+        Playlist playlist = this.playlistRepository.findByIdAndOwner(id, this.userService.me())
+                .orElseThrow(() -> new NotFoundException("Playlist not found!"));
 
-    return this.playlistMapper.toPlaylistReponseDTO(playlist);
-  }
+        return this.playlistMapper.toPlaylistReponseDTO(playlist);
+    }
 
-  @Override
-  public PaginationResponse<PlaylistResponseDTO> getByCurrentUser(
-    Pageable pageable
-  ) {
-    Page<Playlist> currentUserPlaylists =
-      this.playlistRepository.findByOwner(this.userService.me());
-    return PaginationResponseMapper.create(
-      currentUserPlaylists,
-      playlistMapper::toPlaylistReponseDTO
-    );
-  }
+    @Override
+    public PaginationResponse<PlaylistResponseDTO> getByCurrentUser(
+            Pageable pageable) {
+        Page<Playlist> currentUserPlaylists = this.playlistRepository.findByOwner(this.userService.me());
+        return PaginationResponseMapper.create(
+                currentUserPlaylists,
+                playlistMapper::toPlaylistReponseDTO);
+    }
 }
