46a7f551ed51ea2c1f275a6f44f60233d33b2c16
andr30z <andrelp1015@gmail.com>
2023-03-23 21:01:45 -0300

    init frontend environment

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return <main className="text-3xl font-bold underline">test</main>;
+}
