d27c593c101bd09a757b6bbc7e67bf8837efe006
andr30z <51346021+andr30z@users.noreply.github.com>
2023-04-05 11:17:46 -0300

    feat: add dashboard

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="w-full h-full min-h-80vh flex flex-col justify-center items-center gap-2">
+      <h1 className="text-6xl font-bold text-light text-center">Dashboard</h1>
+    </div>
+  );
+}
