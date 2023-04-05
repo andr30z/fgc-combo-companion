8f302e263ad4dd5cc40abba2b6e13e3ac8b4120e
andr30z <51346021+andr30z@users.noreply.github.com>
2023-04-05 15:36:25 -0300

    feat: add hide logic to login button

--- frontend/src/common/components/header/index.tsx
+++ frontend/src/common/components/header/index.tsx
@@ -1,8 +1,14 @@
+'use client';
 import Image from 'next/image';
 import { AppLogo } from '../app-logo';
 import { Button } from '../button';
+import { usePathname, useRouter } from 'next/navigation';
 
 export const Header = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const showLoginButton = pathname !== '/login';
   return (
     <header className="px-10 sm:px20 py-5 bg-dark w-full flex justify-between items-center py-3 h-10vh">
       <Image
@@ -11,12 +17,33 @@ export const Header = () => {
         height={200}
         width={200}
         alt="FGC Combo Companion logo"
-        className="hidden sm:inline-flex"
+        onClick={() => {
+          router.push('/');
+        }}
+        className="hidden sm:inline-flex cursor-pointer"
       />
-      <AppLogo extraStyles="inline-flex sm:hidden" />
+      <AppLogo
+        extraStyles="inline-flex sm:hidden cursor-pointer"
+        onClick={() => {
+          router.push('/');
+        }}
+      />
       <div className="flex flex-row gap-2">
-        <Button text="Login" color="dark" extraStyles="hover:text-primary" />
-        <Button text="Sign Up" color="primary" />
+        {showLoginButton && (
+          <Button
+            renderAsInnerLink
+            href="/login"
+            text="Login"
+            color="dark"
+            extraStyles="hover:text-primary"
+          />
+        )}
+        <Button
+          renderAsInnerLink
+          href="/login"
+          text="Sign Up"
+          color="primary"
+        />
       </div>
     </header>
   );
