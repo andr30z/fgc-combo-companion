aad881d8b01ccd01410d574ddebd806baf312339
andr30z <andrelp1015@gmail.com>
2023-04-06 23:23:05 -0300

    refactor: add extra auth changes to user module

--- backend/src/main/java/com/fgc/combo/companion/model/User.java
+++ backend/src/main/java/com/fgc/combo/companion/model/User.java
@@ -1,11 +1,6 @@
 package com.fgc.combo.companion.model;
 
-import java.time.LocalDateTime;
-
-import org.hibernate.annotations.CreationTimestamp;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -15,10 +10,12 @@ import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Data
 @AllArgsConstructor
@@ -30,12 +27,22 @@ public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
-  @SequenceGenerator(name = "user_seq", sequenceName = "user_seq", allocationSize = 1)
+  @SequenceGenerator(
+    name = "user_seq",
+    sequenceName = "user_seq",
+    allocationSize = 1
+  )
   @Column(name = "id", updatable = false)
   private Long id;
 
   @Column(unique = true)
   private String email;
+
+  @Column(name = "auth_provider")
+  private String authProvider;
+
+  @Column(name = "email_verified")
+  private Boolean emailVerified;
 
   private String name;
 
