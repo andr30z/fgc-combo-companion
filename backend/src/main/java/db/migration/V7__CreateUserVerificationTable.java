f1f3debc5e0d60646ce86c44fd239ba9f4a7a71d
andr30z <51346021+andr30z@users.noreply.github.com>
2023-04-12 12:13:49 -0300

    fix: user verificiation table migration

--- backend/src/main/java/db/migration/V7__CreateUserVerificationTable.java
+++ backend/src/main/java/db/migration/V7__CreateUserVerificationTable.java
@@ -5,31 +5,32 @@ import java.sql.Statement;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
-public class V7_CreateUserVerificationTable extends BaseJavaMigration {
+public class V7__CreateUserVerificationTable extends BaseJavaMigration {
 
   @Override
   public void migrate(Context context) throws Exception {
     Connection connection = context.getConnection();
     Statement statement = connection.createStatement();
     statement.execute(
-      "DROP TYPE IF EXISTS userverificationtypes CASCADE;" +
-      "CREATE TYPE userverificationtypes AS ENUM ('PASSWORD_RESET', 'EMAIL_VERIFICATION');"
+      "DROP TYPE IF EXISTS userverificationtypes CASCADE; " +
+      "CREATE TYPE userverificationtypes AS ENUM ('PASSWORD_RESET', 'EMAIL_VERIFICATION'); "
     );
     statement.execute(
       "CREATE TABLE user_verifications ( " +
-      "id bigint NOT NULL," +
+      "id bigint NOT NULL, " +
       "token character varying(255) NOT NULL, " +
       "created_at timestamp, " +
       "user_id bigint NOT NULL, " +
       "type userverificationtypes NOT NULL, " +
       "expiry_date timestamp NOT NULL, " +
       "CONSTRAINT user_verifications_pkey PRIMARY KEY (id), " +
-      "CONSTRAINT user_fk UNIQUE FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE );"
+      "CONSTRAINT user_fk FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE );"
     );
-    statement.execute(
-      "CREATE SEQUENCE IF NOT EXISTS user_verifications_seq;"
-    );
+    statement.execute("CREATE SEQUENCE IF NOT EXISTS user_verifications_seq;");
 
+    statement.execute(
+      "ALTER TABLE user_verifications ADD CONSTRAINT unique_users_verifications_user_id UNIQUE (user_id);"
+    );
     statement.execute(
       "CREATE UNIQUE INDEX user_verification_token_unique_lower_token_idx on user_verifications (lower(token));"
     );
