8b36aa193ded5f0702b5da2538f5bd2baf8421d3
andr30z <andrelp1015@gmail.com>
2023-05-05 23:25:09 -0300

    build: update next version

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,17 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
+// const path = require('path');
 const nextConfig = {
   experimental: {
     appDir: true,
   },
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname),
-    };
-    return config;
-  },
+  // webpack: (config) => {
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     '@': path.resolve(__dirname),
+  //   };
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
