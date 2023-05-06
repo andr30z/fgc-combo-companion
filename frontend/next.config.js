a730cc1081a965171e51d7e908f132b842689a96
andr30z <andrelp1015@gmail.com>
2023-05-05 22:57:50 -0300

    debug: add import alias config to next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,8 +2,16 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    
   },
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
