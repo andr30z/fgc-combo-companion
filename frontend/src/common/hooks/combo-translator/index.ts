c00e1cfcfa00d34ca0954279b687cd777b355f21
andr30z <andrelp1015@gmail.com>
2023-05-07 00:36:45 -0300

    debug: change string utils name

--- frontend/src/common/hooks/combo-translator/index.ts
+++ frontend/src/common/hooks/combo-translator/index.ts
@@ -13,7 +13,7 @@ import {
   replaceComboWithSpaceFlagWithinBraces,
   replaceSpacesWithinBraces,
   splitMulti,
-} from '@/common/utils/string';
+} from '@/common/utils/_string';
 
 import { useMemo } from 'react';
 
