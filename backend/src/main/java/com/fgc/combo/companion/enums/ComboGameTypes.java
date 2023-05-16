7e77b91b3e3055ba08ad4b96847bb34be5d2d46f
andr30z <51346021+andr30z@users.noreply.github.com>
2023-05-16 14:27:07 -0300

    feat: add street fighter 6 to combo games enum

--- backend/src/main/java/com/fgc/combo/companion/enums/ComboGameTypes.java
+++ backend/src/main/java/com/fgc/combo/companion/enums/ComboGameTypes.java
@@ -3,6 +3,7 @@ package com.fgc.combo.companion.enums;
 public enum ComboGameTypes {
   TEKKEN_7,
   SFV,
+  STREET_FIGHTER_6,
   GUILTY_GEAR_STRIVE,
   KOF_XV;
 
@@ -10,10 +11,19 @@ public enum ComboGameTypes {
 
     public static final String TEKKEN_7 = "TEKKEN_7";
     public static final String SFV = "SFV";
+    public static final String STREET_FIGHTER_6 = "STREET_FIGHTER_6";
     public static final String GUILTY_GEAR_STRIVE = "GUILTY_GEAR_STRIVE";
-
     public static final String KOF_XV = "KOF_XV";
 
-    public static final String ALL_GAME_TYPES = TEKKEN_7 + ", " + SFV + ", " + GUILTY_GEAR_STRIVE + ", " + KOF_XV;
+    public static final String ALL_GAME_TYPES =
+      TEKKEN_7 +
+      ", " +
+      SFV +
+      ", " +
+      GUILTY_GEAR_STRIVE +
+      ", " +
+      KOF_XV +
+      ", " +
+      STREET_FIGHTER_6;
   }
 }
