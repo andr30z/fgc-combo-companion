0bae676be2769bc56fc1109c5dec54babbc17d96
andr30z <andrelp1015@gmail.com>
2023-05-31 21:32:48 -0300

    fix: try new setKey definition

--- backend/src/main/java/com/fgc/combo/companion/utils/SecurityCipher.java
+++ backend/src/main/java/com/fgc/combo/companion/utils/SecurityCipher.java
@@ -10,26 +10,30 @@ import javax.crypto.spec.SecretKeySpec;
 
 import com.fgc.combo.companion.exception.SecurityCipherDecodeException;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class SecurityCipher {
 
   private static final String KEYVALUE = "secureKey";
-  private static SecretKeySpec secretKey;
   private static byte[] key;
+  private static SecretKeySpec secretKey = setKey();
 
   private SecurityCipher() {
     throw new AssertionError("Static!");
   }
 
-  public static void setKey() {
+  public static SecretKeySpec setKey() {
     MessageDigest sha;
     try {
       key = KEYVALUE.getBytes(StandardCharsets.UTF_8);
       sha = MessageDigest.getInstance("SHA-1");
       key = sha.digest(key);
       key = Arrays.copyOf(key, 16);
-      secretKey = new SecretKeySpec(key, "AES");
+      return new SecretKeySpec(key, "AES");
     } catch (NoSuchAlgorithmException e) {
       e.printStackTrace();
+      return null;
     }
   }
 
@@ -37,9 +41,9 @@ public class SecurityCipher {
     if (strToEncrypt == null) return null;
 
     try {
-      setKey();
       Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
       cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+      log.info("Encryption Key: {}", key.toString());
       return Base64
         .getEncoder()
         .encodeToString(
@@ -62,7 +66,7 @@ public class SecurityCipher {
         cipher.doFinal(Base64.getDecoder().decode(strToDecrypt))
       );
     } catch (Exception e) {
-      e.printStackTrace();
+      log.error("Error while decrypting token: {},  encryption key: {} stackTrace: {}", Base64.getDecoder().decode(strToDecrypt), key.toString(), e.getMessage());
     }
     if (throwException) throw new SecurityCipherDecodeException(
       "Malformed token"
