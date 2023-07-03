aa387de50f16c5398bd4eedc74003e62268aa0d7
andr30z <51346021+andr30z@users.noreply.github.com>
2023-07-03 13:37:24 -0300

    feat: add api pings on frontend

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -8,6 +8,7 @@ import { Roboto } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 import { RefreshToken } from '@/modules/home/refresh-token';
+import { getFgcApiInstance } from '@/common/services/fgc-api';
 export const metadata = {
   title: 'FGC Combo Companion',
   description: 'FGC COMBO COMPANION',
@@ -27,6 +28,10 @@ export default function RootLayout({
   children: React.ReactNode;
   session: Session | null;
 }) {
+  const fgcApi = getFgcApiInstance();
+  //this request is only beign made because I'm deploying the backend on render.com on free plan
+  //so maybe when some user access the page for the first time the api maybe idling
+  fgcApi.get('/'); //pinging api
   return (
     <html lang="en" className={`${roboto.variable}`}>
       <body className="min-h-screen bg-dark">
--- frontend/src/modules/login-page/login-form.tsx
+++ frontend/src/modules/login-page/login-form.tsx
@@ -6,6 +6,7 @@ import { Link } from '@/common/components/link';
 import { LoadingBackdrop } from '@/common/components/loading-backdrop';
 import { useBoolean } from '@/common/hooks/boolean';
 import { useForm } from '@/common/hooks/form';
+import { fgcApi } from '@/common/services/fgc-api';
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { toast } from 'react-hot-toast';
@@ -59,6 +60,14 @@ export const LoginForm = () => {
 
           if (hasError) {
             return;
+          }
+          try {
+            // eslint-disable-next-line no-console
+            console.log('Pinging fgc api...');
+            await fgcApi.get('/');
+          } catch (error) {
+            // eslint-disable-next-line no-console
+            console.error(error);
           }
           startLoading();
           const data = await signIn('fgc-email-password', {
