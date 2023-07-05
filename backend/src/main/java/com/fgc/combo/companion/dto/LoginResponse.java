c2ee148e361f042b5c59e42ef280a878a6aa8f82
andr30z <andrelp1015@gmail.com>
2023-07-05 20:00:28 -0300

    feat: add MeDto to hide oAuthId

--- backend/src/main/java/com/fgc/combo/companion/dto/LoginResponse.java
+++ backend/src/main/java/com/fgc/combo/companion/dto/LoginResponse.java
@@ -1,6 +1,5 @@
 package com.fgc.combo.companion.dto;
 
-import com.fgc.combo.companion.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -11,7 +10,7 @@ public class LoginResponse {
   private SuccessFailure status;
   private String message;
 
-  private User user;
+  private UserDto user;
 
   public enum SuccessFailure {
     SUCCESS,
