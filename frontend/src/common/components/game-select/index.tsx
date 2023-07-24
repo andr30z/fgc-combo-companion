fcf182e5cbdc6dfd289f7c26e02601f166886ad9
andr30z <andrelp1015@gmail.com>
2023-07-23 22:31:52 -0300

    feat: add next fighting game to select game component.  ps: FUCK GUILTY GEAR STRIVE COMBO NOTATION

--- frontend/src/common/components/game-select/index.tsx
+++ frontend/src/common/components/game-select/index.tsx
@@ -5,7 +5,7 @@ import type { FC } from 'react';
 
 type OnSelectFunction = (option: GameTypes) => void;
 
-const Option: React.FC<{
+const Option: FC<{
   url: string;
   showSoonMessage?: boolean;
   option: GameTypes;
@@ -15,13 +15,13 @@ const Option: React.FC<{
   const selected = selectedOption === option;
   return (
     <div
-      className={`group overflow-hidden cursor-pointer relative rounded-lg border-4 solid  ${
+      className={`min-h-[180px] min-w-[280px] group overflow-hidden cursor-pointer relative rounded-lg border-4 solid  ${
         selected ? 'border-primary' : 'border-transparent'
       }`}
       onClick={() => (showSoonMessage ? null : onSelect(option))}
     >
       <Image
-        alt="next step"
+        alt="Fighting game image"
         className="h-full w-full scale-100 group-hover:scale-110 ease-in duration-500"
         height={230}
         width={300}
@@ -32,7 +32,7 @@ const Option: React.FC<{
       )}
       {showSoonMessage && (
         <div className="absolute top-0 w-full h-full flex items-center justify-center">
-          <h4 className="text-light font-primary font-semibold text-sm sm:text-lg bg-dark p-1 rounded-lg opacity-60">
+          <h4 className="text-light font-primary font-semibold text-sm sm:text-lg bg-dark p-1 rounded-lg opacity-60 select-none">
             SOON
           </h4>
         </div>
@@ -53,7 +53,7 @@ export const GameSelect: FC<{
           Select a game:
         </span>
       </label>
-      <div className="flex flex-row flex-wrap sm:flex-nowrap gap-4 w-full">
+      <div className="flex flex-row gap-4 pr-16 w-full overflow-x-auto overflow-y-hidden no-scrollbar relative">
         <Option
           option={GameTypes.TEKKEN_7}
           selectedOption={selectedOption}
@@ -71,6 +71,17 @@ export const GameSelect: FC<{
           option={GameTypes.GUILTY_GEAR_STRIVE}
           onSelect={onSelect}
           url="/guilty-gear-strive/guilty-gear-strive-select.jpg"
+        />
+        <Option
+          selectedOption={selectedOption}
+          option={GameTypes.DB_FIGHTERZ}
+          onSelect={onSelect}
+          url="/dragon-ball-fighterz/db-fighterz-select.png"
+          showSoonMessage
+        />
+        <div
+          style={{ boxShadow: '50px 0px 30px 40px #000' }}
+          className="min-h-full min-w-[.01px] xl:hidden  sticky right-0"
         />
       </div>
     </div>
