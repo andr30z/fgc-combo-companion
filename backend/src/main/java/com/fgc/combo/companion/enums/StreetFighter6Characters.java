7f5672e047580ced0c5a504648c63876d8a86b3f
andr30z <andrelp1015@gmail.com>
2023-07-31 21:23:45 -0300

    feat: add validations to new combo table columns

---
+++ backend/src/main/java/com/fgc/combo/companion/enums/StreetFighter6Characters.java
@@ -0,0 +1,23 @@
+package com.fgc.combo.companion.enums;
+
+public enum StreetFighter6Characters {
+  BLANKA,
+  CAMMY,
+  CHUN_LI,
+  DEE_JAY,
+  DHALSIM,
+  E_HONDA,
+  GUILE,
+  JAMIE,
+  JP,
+  JURI,
+  KEN,
+  KIMBERLY,
+  LILY,
+  LUKE,
+  MANON,
+  MARISA,
+  RYU,
+  ZANGIEF,
+  RASHID,
+}
---
+++ backend/src/main/java/com/fgc/combo/companion/utils/ComboCharactersValidation.java
@@ -0,0 +1,53 @@
+package com.fgc.combo.companion.utils;
+
+import com.fgc.combo.companion.enums.ComboGameTypes;
+import com.fgc.combo.companion.enums.GuiltyGearStriveCharacters;
+import com.fgc.combo.companion.enums.StreetFighter6Characters;
+import com.fgc.combo.companion.enums.Tekken7Characters;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class ComboCharactersValidation {
+
+  private static final Map<ComboGameTypes, List<String>> validComboCharacters = Map.ofEntries(
+    Map.entry(
+      ComboGameTypes.TEKKEN_7,
+      Arrays
+        .asList(Tekken7Characters.values())
+        .stream()
+        .map(Tekken7Characters::name)
+        .toList()
+    ),
+    Map.entry(
+      ComboGameTypes.STREET_FIGHTER_6,
+      Arrays
+        .asList(StreetFighter6Characters.values())
+        .stream()
+        .map(StreetFighter6Characters::name)
+        .toList()
+    ),
+    Map.entry(
+      ComboGameTypes.GUILTY_GEAR_STRIVE,
+      Arrays
+        .asList(GuiltyGearStriveCharacters.values())
+        .stream()
+        .map(GuiltyGearStriveCharacters::name)
+        .toList()
+    )
+  );
+
+  public static boolean isComboCharacterValid(
+    String comboCharacter,
+    String comboGameType
+  ) {
+    try {
+      return validComboCharacters
+        .get(ComboGameTypes.valueOf(comboGameType))
+        .contains(comboCharacter);
+    } catch (Exception e) {
+      //heheheboy
+      return false;
+    }
+  }
+}
