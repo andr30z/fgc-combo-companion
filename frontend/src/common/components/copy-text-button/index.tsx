a656327aa74a9e0077d3238558e36a1fd4408b37
andr30z <andrelp1015@gmail.com>
2023-08-19 20:16:25 -0300

    feat: create copy-text-button

---
+++ frontend/src/common/components/copy-text-button/index.tsx
@@ -0,0 +1,43 @@
+import { toast } from 'react-hot-toast';
+import { AiOutlineCopy } from 'react-icons/ai';
+import { Button, ButtonProps } from '../button';
+
+interface CopyTextButtonProps extends Partial<ButtonProps> {
+  textToCopy: string;
+  isAppUrlCopy?: boolean;
+  buttonIcon?: React.ReactNode;
+}
+
+export const CopyTextButton: React.FC<CopyTextButtonProps> = ({
+  textToCopy,
+  isAppUrlCopy = false,
+  buttonIcon = <AiOutlineCopy size={17} />,
+  ...btnProps
+}) => {
+  const getCopyText = () => {
+    if (!isAppUrlCopy) {
+      return textToCopy;
+    }
+    return `${
+      process.env.NODE_ENV === 'production'
+        ? 'https://app.fgc-combo-companion.xyz'
+        : 'http://localhost:3000'
+    }/${textToCopy}`;
+  };
+  return (
+    <Button
+      color="primary"
+      leftIcon={buttonIcon}
+      onClick={() => {
+        const element = document.createElement('textarea');
+        element.value = getCopyText();
+        document.body.appendChild(element);
+        element.select();
+        document.execCommand('copy');
+        document.body.removeChild(element);
+        toast.success('The share link was copied to the clipboard');
+      }}
+      {...btnProps}
+    />
+  );
+};
