7c68a7a2e9f14349262036d234d121010fb909a0
asdas <asdasdasd@adsdasdasd>
2025-02-10 19:52:56 -0300

    fix: enum validator

--- backend/src/main/java/com/fgc/combo/companion/config/SecurityConfig.java
+++ backend/src/main/java/com/fgc/combo/companion/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.fgc.combo.companion.config;
 
-import com.fgc.combo.companion.service.impl.CustomUserDetailsServiceImpl;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -18,12 +16,22 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.fgc.combo.companion.service.impl.CustomUserDetailsServiceImpl;
+import com.fgc.combo.companion.validation.ValueOfEnumValidator;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
 @EnableWebSecurity
 @Slf4j
 public class SecurityConfig {
 
   private static final int PASSWORD_STRENGTH = 10;
+
+  @Bean
+  ValueOfEnumValidator valueOfEnumValidator() {
+    return new ValueOfEnumValidator();
+  }
 
   // @Autowired
   // private CustomUserDetailsServiceImpl customUserDetailsService;
@@ -38,117 +46,111 @@ public class SecurityConfig {
     return new TokenAuthenticationFilter();
   }
 
-  //   @Override
-  //   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-  //     auth
-  //       .userDetailsService(customUserDetailsService)
-  //       .passwordEncoder(passwordEncoder());
-  //   }
+  // @Override
+  // protected void configure(AuthenticationManagerBuilder auth) throws Exception
+  // {
+  // auth
+  // .userDetailsService(customUserDetailsService)
+  // .passwordEncoder(passwordEncoder());
+  // }
 
   @Bean
   AuthenticationManager authenticationManager(
-    HttpSecurity http,
-    PasswordEncoder passwordEncoder,
-    CustomUserDetailsServiceImpl customUserDetailsService
-  ) throws Exception {
+      HttpSecurity http,
+      PasswordEncoder passwordEncoder,
+      CustomUserDetailsServiceImpl customUserDetailsService) throws Exception {
     return http
-      .getSharedObject(AuthenticationManagerBuilder.class)
-      .userDetailsService(customUserDetailsService)
-      .passwordEncoder(passwordEncoder)
-      .and()
-      .build();
+        .getSharedObject(AuthenticationManagerBuilder.class)
+        .userDetailsService(customUserDetailsService)
+        .passwordEncoder(passwordEncoder)
+        .and()
+        .build();
   }
 
   @Bean
   SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http
-      .cors()
-      .and()
-      // .anonymous()
-      // .disable()
-      .sessionManagement()
-      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-      .and()
-      .csrf()
-      .disable()
-      .formLogin()
-      .disable()
-      .httpBasic()
-      .disable()
-      .exceptionHandling()
-      .authenticationEntryPoint(new RestAuthenticationEntryPoint())
-      .and()
-      .authorizeHttpRequests()
-      .requestMatchers(
-        "/",
-        // -- Swagger UI v2
-        "/v2/api-docs",
-        "/swagger-resources",
-        "/swagger-resources/**",
-        "/configuration/ui",
-        "/configuration/security",
-        "/swagger-ui.html",
-        "/webjars/**",
-        // -- Swagger UI v3 (OpenAPI)
-        "/v3/api-docs/**",
-        "/swagger-ui/**",
-        "/actuator",
-        // other public endpoints of your API may be appended to this array
+        .cors()
+        .and()
+        // .anonymous()
+        // .disable()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .csrf()
+        .disable()
+        .formLogin()
+        .disable()
+        .httpBasic()
+        .disable()
+        .exceptionHandling()
+        .authenticationEntryPoint(new RestAuthenticationEntryPoint())
+        .and()
+        .authorizeHttpRequests()
+        .requestMatchers(
+            "/",
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**",
+            "/actuator",
+            // other public endpoints of your API may be appended to this array
 
-        "/error",
-        "/favicon.ico",
-        "/**/*.png",
-        "/**/*.gif",
-        "/**/*.svg",
-        "/**/*.jpg",
-        "/**/*.html",
-        "/**/*.css",
-        "/**/*.js"
-      )
-      .permitAll()
-      .and()
-      .authorizeHttpRequests()
-      .requestMatchers(
-        HttpMethod.POST,
-        "/api/v1/users/login",
-        "/api/v1/users/refresh",
-        "/api/v1/users",
-        "/api/v1/users/oauth/login",
-        "/api/v1/users/email-verification-solicitation",
-        "/api/v1/users/password-change-solicitation"
-      )
-      .permitAll()
-      .requestMatchers(
-        HttpMethod.GET,
-        "/api/v1/users/me",
-        "/api/v1/playlists/me"
-      )
-      .authenticated()
-      .requestMatchers(
-        HttpMethod.PATCH,
-        "/api/v1/users/password-change",
-        "/api/v1/users/email-verification"
-      )
-      .permitAll()
-      .requestMatchers(
-        HttpMethod.GET,
-        "/api/v1/profile/{userId}",
-        "/api/v1/users/verification/**",
-        "/api/v1/users/{userId}",
-        "/api/v1/playlists/{playlistId}",
-        "/api/v1/combos/{comboId}",
-        "/api/v1/combos/users/{userId}",
-        "/api/v1/playlists/users/{userId}"
-      )
-      .permitAll()
-      .anyRequest()
-      .authenticated()
-      .and()
-      .addFilterBefore(
-        tokenAuthenticationFilter(),
-        UsernamePasswordAuthenticationFilter.class
-      )
-      .build();
+            "/error",
+            "/favicon.ico",
+            "/**/*.png",
+            "/**/*.gif",
+            "/**/*.svg",
+            "/**/*.jpg",
+            "/**/*.html",
+            "/**/*.css",
+            "/**/*.js")
+        .permitAll()
+        .and()
+        .authorizeHttpRequests()
+        .requestMatchers(
+            HttpMethod.POST,
+            "/api/v1/users/login",
+            "/api/v1/users/refresh",
+            "/api/v1/users",
+            "/api/v1/users/oauth/login",
+            "/api/v1/users/email-verification-solicitation",
+            "/api/v1/users/password-change-solicitation")
+        .permitAll()
+        .requestMatchers(
+            HttpMethod.GET,
+            "/api/v1/users/me",
+            "/api/v1/playlists/me")
+        .authenticated()
+        .requestMatchers(
+            HttpMethod.PATCH,
+            "/api/v1/users/password-change",
+            "/api/v1/users/email-verification")
+        .permitAll()
+        .requestMatchers(
+            HttpMethod.GET,
+            "/api/v1/profile/{userId}",
+            "/api/v1/users/verification/**",
+            "/api/v1/users/{userId}",
+            "/api/v1/playlists/{playlistId}",
+            "/api/v1/combos/{comboId}",
+            "/api/v1/combos/users/{userId}",
+            "/api/v1/playlists/users/{userId}")
+        .permitAll()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .addFilterBefore(
+            tokenAuthenticationFilter(),
+            UsernamePasswordAuthenticationFilter.class)
+        .build();
   }
 
   @Profile("default")
@@ -160,11 +162,11 @@ public class SecurityConfig {
         log.info("Registering DEV CORS filter...");
 
         registry
-          .addMapping("/**")
-          .allowCredentials(true)
-          .allowedHeaders("*")
-          .allowedMethods("*")
-          .allowedOriginPatterns("*");
+            .addMapping("/**")
+            .allowCredentials(true)
+            .allowedHeaders("*")
+            .allowedMethods("*")
+            .allowedOriginPatterns("*");
       }
     };
   }
@@ -178,11 +180,11 @@ public class SecurityConfig {
         log.info("Registering PRODUCTION CORS filter...");
 
         registry
-          .addMapping("/**")
-          .allowCredentials(true)
-          .allowedHeaders("*")
-          .allowedMethods("*")
-          .allowedOriginPatterns("https://*.fgc-combo-companion.xyz");
+            .addMapping("/**")
+            .allowCredentials(true)
+            .allowedHeaders("*")
+            .allowedMethods("*")
+            .allowedOriginPatterns("https://*.fgc-combo-companion.xyz");
       }
     };
   }
